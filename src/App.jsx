33bc698b74388d1268104ecc1c552a32356c1e81
fery-blg <ferielblg5@gmail.com>
2023-12-26 11:13:59 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@
 import MyClassComponent from "./components/MyClassComponets";
 
 function App() {
-   const setCount = (i,f)=>{
-   f({count: i}); 
-    console.log(this.state.count ,"this count value updated with  function came from props ")
-   }
+
 
     return <MyClassComponent  />
 }
