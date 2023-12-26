2cfd99e9b4a61d955fbe203b4e7c352f7ad30077
fery-blg <ferielblg5@gmail.com>
2023-12-26 12:08:03 +0100

    how to map in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,25 @@
 import MyClassComponent from "./components/MyClassComponets";
 
 function App() {
+    const kvArray = [
+        { key: 1, value: 10 },
+        { key: 2, value: 20 },
+        { key: 3, value: 30 }
+      ];
+    return (
+    <>
+    <MyClassComponent  />
 
 
-    return <MyClassComponent  />
-}
+   
+    { kvArray.map((e)=> (
+        <div key={e.key} >{e.value}</div>
+    ))}
+    </>)}
+
+   
+
+
+   
+
 export default App
